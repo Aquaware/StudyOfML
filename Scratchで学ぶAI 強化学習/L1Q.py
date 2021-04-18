f1d998adaa6f3a801a206d25cfe64a3b0050265b
RegimeChanger <81889588+RegimeChanger@users.noreply.github.com>
2021-04-18 09:41:47 +0900

    update
    update

--- Scratchで学ぶAI 強化学習/L1Q.py
+++ Scratchで学ぶAI 強化学習/L1Q.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+class Learning():
+    
+    def __init__(self):
+        self.reward_left = 0.5
+        self.reward_right = 0.5
+        self.randomness = 0.2
+        self.learning_factor = 0.1
 def main():
     
     return
